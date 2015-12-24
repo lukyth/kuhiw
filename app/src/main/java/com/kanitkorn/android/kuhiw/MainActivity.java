cff06d43182a873533c9d4ce88ab788cbe3c5031
Kanitkorn S <k.sujautra@gmail.com>
2015-12-24 07:43:53 +0700

    Modify comment

--- app/src/main/java/com/kanitkorn/android/kuhiw/MainActivity.java
+++ app/src/main/java/com/kanitkorn/android/kuhiw/MainActivity.java
@@ -12,8 +12,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.kanitkorn.android.kuhiw.dummy.DummyContent;
-
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, RestaurantFragment.OnListFragmentInteractionListener {
 
